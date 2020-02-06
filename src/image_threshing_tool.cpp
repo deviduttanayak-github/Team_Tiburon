4046cfeed36abcb44cf3522db6c74da01ee39f6d
deviduttanayak-github <nayak.devidutta17@gmail.com>
2020-02-07 01:22:18 +0530

    GUI task added

--- src/image_threshing_tool.cpp
+++
@@ -1,9 +0,0 @@
-#include "image_threshing_tool/image_threshing_tool.h"
-#include "ui_image_threshing_tool.h"
-
-ImageThreshingTool::ImageThreshingTool(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::ImageThreshingTool) {
-  ui->setupUi(this);
-  }
-
-ImageThreshingTool::~ImageThreshingTool() { delete ui; }
